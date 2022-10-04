0a11f7e99726326f2a7a6d520e83b19f78f41f9f
Atanu Paul <atanupaul2411@gmail.com>
2022-10-04 10:25:48 +0530

    prime number pushed

---
+++ Java files/PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.util;
+import java.util.Scanner;
+
+// code 
+public class prime{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <=n; i++) {
+            System.out.println(i + " " + isPrime(i));
+        }
+    }
+
+    public static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        
+
+        int c = 2;
+        while(c * c <= n){
+            if(n % c == 0){
+                return false;
+            }
+            c++;
+        }
+        return true;
+    }
+
+}
